ede80e427e86e9923737f56d41c8e66b923d60e6
YeriNbow <jwjln3126@naver.com>
2020-11-07 15:32:00 +0900

    Update statiz.py

--- statiz.py
+++ statiz.py
@@ -17,6 +17,7 @@ XPATH = '//*[@id="mytable"]/tbody'
 BASE_URL = 'http://www.statiz.co.kr/stat.php?opt=0&sopt=0&re=0&ys=2020&ye=2020&se=0&te=&tm=&ty=0&qu=auto&' \
            'po=0&as=&ae=&hi=&un=&pl=&da=1&o1=WAR_ALL_ADJ&de=1&lr=0&tr=&cv=&ml=1&sn=1000&si=&cn=1000'
 
+# crawling
 driver.get(BASE_URL)
 driver.implicitly_wait(5)
 
@@ -25,6 +26,7 @@ page = BeautifulSoup(html, 'html.parser')
 
 driver.close()
 
+# regex
 birth_regex = re.compile(r'\d{4}-\d{2}-\d{2}')
 name_regex = re.compile(r'\d+(.*\d{2})?')
 position_regex = re.compile(r'(1B|2B|3B|SS|C|LF|RF|CF|DH)$')
@@ -33,12 +35,13 @@ trs = page.findAll('tr')
 df = pd.DataFrame(columns=['name', 'birth', 'team', 'position'])
 
 for tr in trs:
-    birth = ''.join(birth_regex.findall(tr.find('a')['href']))
+    tmp = tr
     tr = pd.Series(tr.text.strip().replace('\n', '').split(' '))
 
     if len(tr) == 1:
         continue
 
+    birth = ''.join(birth_regex.findall(tmp.find('a')['href']))
     name = ''.join(name_regex.findall(tr[0]))
     position = ''.join(position_regex.findall(tr[0]))
 
@@ -54,8 +57,8 @@ for tr in trs:
     df = df.append(tr, ignore_index=True).dropna(axis=1)
 
 
-df.drop([0, 53], axis=1, inplace=True)
-df.columns = ['이름', '생년월일', '팀', '포지션', 'WAR', 'G', '타석', '타수', '득점', '안타', '2루타', '3루타',
-              '홈런', '루타', '타점', '도루', '도실', '볼넷', '사구', '고4', '삼진', '병살', '희타', '희비', 'WPA']
+df.drop([0, 53], axis=1, inplace=True)  # remove useless / duplicate data
+df.columns = ['Name', 'Birth', 'Team', 'Position', 'WAR', 'G', 'PA', 'AB', 'R', 'H', '2B', '3B',
+              'HR', 'TB', 'RBI', 'SB', 'CB', 'BB', 'HBP', 'IBB', 'SO', 'DP', 'SH', 'SF', 'WPA']
 
 print(df)
