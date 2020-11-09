d38ff3a8641ee78417ddc7af028b575e0e74255a
YeriNbow <jwjln3126@naver.com>
2020-11-09 18:52:25 +0900

    Update statiz.py

--- statiz.py
+++ statiz.py
@@ -5,71 +5,101 @@ import pandas as pd
 import utils
 
 
-WD_PATH = 'D:/IT/mywork/chromedriver.exe'
-OPTIONS = webdriver.ChromeOptions()
-OPTIONS.add_argument('--headless')
-OPTIONS.add_argument('--no-sandbox')
-OPTIONS.add_argument('--disable-dev-shm-usage')
-OPTIONS.add_argument('disable-gpu')
+class StatizCrawler:
+    WD_PATH = 'D:/IT/mywork/chromedriver.exe'
 
-driver = webdriver.Chrome(WD_PATH, options=OPTIONS)
+    OPTIONS = webdriver.ChromeOptions()
+    OPTIONS.add_argument('--headless')
+    OPTIONS.add_argument('--no-sandbox')
+    OPTIONS.add_argument('--disable-dev-shm-usage')
+    OPTIONS.add_argument('disable-gpu')
 
-U = 'http://www.statiz.co.kr/stat.php?opt=0&sopt=0&re=0&ys='
-R = '&ye='
-L = '&se=0&te=&tm=&ty=0&qu=auto&po=0&as=&ae=&hi=&un=&pl=&da=1&o1='
-Batter = 'WAR_ALL_ADJ&de=1&lr=0&tr=&cv=&ml=1&sn=1000&si=&cn=500'
-Pitcher = 'WAR&o2=OutCount&de=1&lr=0&tr=&cv=&ml=1&sn=30&si=&cn=500'
+    U = 'http://www.statiz.co.kr/stat.php?opt=0&sopt=0&re=0&ys='
+    R = '&ye='
+    L = '&se=0&te=&tm=&ty=0&qu=auto&po=0&as=&ae=&hi=&un=&pl=&da=1&o1='
+    Batter = 'WAR_ALL_ADJ&de=1&lr=0&tr=&cv=&ml=1&sn=1000&si=&cn=500'
+    Pitcher = 'WAR&o2=OutCount&de=1&lr=0&tr=&cv=&ml=1&sn=30&si=&cn=500'
 
-XPATH = '//*[@id="mytable"]/tbody'
+    XPATH = '//*[@id="mytable"]/tbody'
 
-# regex
-birth_regex = re.compile(r'\d{4}-\d{2}-\d{2}')
-name_regex = re.compile(r'\d+(.*\d{2})?')
-position_regex = re.compile(r'(1B|2B|3B|SS|C|LF|RF|CF|DH|P)$')
+    def __init__(self, year, pos='B'):
+        """
+        :param year: This parameter determines a year to crawl. (1982 ~ present)
+        :param pos: This parameter determines crawling data type. ('B' : Batter, 'P' : Pitcher)
+        """
+        self.pos = pos
 
-df = pd.DataFrame(columns=['name', 'birth', 'team', 'position'])
+        self.birth_regex = re.compile(r'\d{4}-\d{2}-\d{2}')
+        self.name_regex = re.compile(r'\d+(.*\d{2})?')
+        self.position_regex = re.compile(r'(1B|2B|3B|SS|C|LF|RF|CF|DH|P)$')
 
-for year in range(1982, 2021):
-    print('Now Scraping : {}'.format(year))
+        self.statiz_df = pd.DataFrame(columns=['name', 'birth', 'team', 'position'])
 
-    BASE_URL = U + str(year) + R + str(year) + L + Batter
-    driver.get(BASE_URL)
-    driver.implicitly_wait(5)
+        if pos == 'B':
+            self.base_url = self.U + str(year) + self.R + str(year) + self.L + self.Batter
+        else:
+            self.base_url = self.U + str(year) + self.R + str(year) + self.L + self.Pitcher
 
-    html = driver.find_element_by_xpath(XPATH).get_attribute("innerHTML")
-    page = BeautifulSoup(html, 'html.parser')
-    trs = page.findAll('tr')
+    def crawl(self):
+        driver = webdriver.Chrome(self.WD_PATH, options=self.OPTIONS)
+        driver.get(self.base_url)
+        driver.implicitly_wait(5)
 
-    count = 1
-    for tr in trs:
-        utils.progress_bar(count, len(trs))
-        count += 1
+        html = driver.find_element_by_xpath(self.XPATH).get_attribute("innerHTML")
+        page = BeautifulSoup(html, 'html.parser')
+        trs = page.findAll('tr')
 
-        tmp = tr
-        tr = pd.Series(tr.text.strip().replace('\n', '').split(' '))
+        count = 1
+        for tr in trs:
+            utils.progress_bar(count, len(trs))
+            count += 1
 
-        if len(tr) == 1:
-            continue
+            tmp = tr
+            tr = pd.Series(tr.text.strip().replace('\n', '').split(' '))
 
-        birth = ''.join(birth_regex.findall(tmp.find('a')['href']))
-        name = ''.join(name_regex.findall(tr[0]))
-        position = ''.join(position_regex.findall(tr[0]))
+            if len(tr) == 1:
+                continue
 
-        team_regex = re.compile(name + '(.*)?' + position)
-        team = ''.join(team_regex.findall(tr[0]))
+            birth = ''.join(self.birth_regex.findall(tmp.find('a')['href']))
+            name = ''.join(self.name_regex.findall(tr[0]))
+            position = ''.join(self.position_regex.findall(tr[0]))
 
-        tr = tr.replace('', pd.NA)
-        tr['name'] = name
-        tr['birth'] = birth
-        tr['team'] = team
-        tr['position'] = position
+            team_regex = re.compile(name + '(.*)?' + position)
+            team = ''.join(team_regex.findall(tr[0]))
 
-        df = df.append(tr, ignore_index=True).dropna(axis=1)
+            tr = tr.replace('', pd.NA)
+            tr['name'] = name
+            tr['birth'] = birth
+            tr['team'] = team
+            tr['position'] = position
 
-driver.quit()
-df.drop([0, 53], axis=1, inplace=True)
-df.columns = ['Name', 'Birth', 'Team', 'Position', 'WAR', 'G', 'PA', 'AB', 'R', 'H', '2B', '3B',
-              'HR', 'TB', 'RBI', 'SB', 'CB', 'BB', 'HBP', 'IBB', 'SO', 'DP', 'SH', 'SF']
+            self.statiz_df = self.statiz_df.append(tr, ignore_index=True).dropna(axis=1)
 
-print(df)
-df.to_excel('D:/IT/mywork/kbo_batter.xlsx', encoding='utf-8', index=False)
+        driver.quit()
+
+        self.statiz_df = self.statiz_df.drop([0, 53], axis=1, inplace=True)
+        self.set_columns()
+
+        return self.statiz_df
+
+    def set_columns(self):
+        if self.pos == 'B':
+            self.statiz_df.columns = ['Name', 'Birth', 'Team', 'Position', 'WAR', 'G', 'PA', 'AB', 'R', 'H', '2B', '3B',
+                                      'HR', 'TB', 'RBI', 'SB', 'CB', 'BB', 'HBP', 'IBB', 'SO', 'DP', 'SH', 'SF']
+        else:
+            self.statiz_df.columns = ['']
+
+
+if __name__ == '__main__':
+    file_path = 'D:/IT/mywork/kbo_batter.xlsx'
+    baseball = pd.DataFrame()
+
+    for year in range(1982, 2021):
+        sc = StatizCrawler(year, pos='B')
+        print('Now Scraping : {}'.format(year))
+        baseball.append(sc.crawl(), ignore_index=True)
+
+    # baseball.drop([0, 53], axis=1, inplace=True)
+
+    print(baseball)
+    baseball.to_excel(file_path, encoding='utf-8', index=False)
