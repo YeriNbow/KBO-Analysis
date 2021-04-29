4db32890bc414f3a54a0563c35dcd9d4c889df2c
YeriNbow <jwjln3126@naver.com>
2021-04-29 12:27:16 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-def progress_bar(count, length):
-    percent = '{0:.1f}'.format(100 * (count / float(length)))
-    fillin = int(round(100 * count / float(length)))
-    bar = '#' * fillin + '-' * (100 - fillin)
-
-    print(f'\r[%s] %s%s Complete' % (bar, percent, '%'), end='')
-    if count == length:
-        print()
